fe58239b99b3160a3a46633d936b40c5a6e49026
kiranloka <kirankumarloka@gmail.com>
2023-11-27 09:13:34 +0530

    Changes have been made

---
+++ fibonacciUsigRecursion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+Fibonacci(int n){
+    if(n<=1){
+        return n;
+    }
+    else{
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
+
+int main(){
+    int n;
+    printf("Enter the number of terms: ");
+    scanf("%d",&n);
+    for(int i=0;i<=n;i++){
+        printf("%d",fibonacci(i));
+    }
+    return 0;
+}
