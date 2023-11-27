4352123347e7b8aa3c49181ec49bec26761ecaa7
kiranloka <kirankumarloka@gmail.com>
2023-11-27 14:28:43 +0530

    bubble Sort

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,1 +1,36 @@
-void bubbleSort(int arr)
+void bubbleSort(int arr[],int n){
+  for(int i=0;i<n-1;i++){
+    for(int j=0;j<n-i-1;j++){
+      if(arr[j]>arr[j+1]){
+        int temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+      }
+    }
+  }
+}
+
+void printArray(int arr[],int size){
+  for(int i=0;i<size;i++){
+    printf("%d ",arr[i]);
+  }
+  printf("\n");
+}
+
+int main(){
+  int size;
+  printf("Enter the number of elements in the array: ");
+  scanf("%d",&size);
+
+  int arr[size];
+
+  printf("Enter the elements of the array: ");
+  for(int i=0;i<size;i++){
+    scanf("%d",&arr[i]);
+  }
+
+  bubbleSort(arr,size);
+
+  printf("sorted Array: \n");
+  printArray(arr,size);
+}
